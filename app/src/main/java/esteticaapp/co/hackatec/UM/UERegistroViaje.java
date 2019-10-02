9ef628465024b92f35a132c05a20110892ef1ff3
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-10-02 02:17:19 -0500

    Add files via upload
    Activity del registro de viajes

---
+++ app/src/main/java/esteticaapp/co/hackatec/UM/UERegistroViaje.java
@@ -0,0 +1,14 @@
+package esteticaapp.co.hackatec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UERegistroViaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ueregistro_viaje);
+
+    }
+}
