c9c57ab3f018e35fd887b3ca25b8180c33ec45ac
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-10-02 13:47:48 -0500

    Update UEInicio.java

--- app/src/main/java/esteticaapp/co/hackatec/UE/UEInicio.java
+++ app/src/main/java/esteticaapp/co/hackatec/UE/UEInicio.java
@@ -1,6 +1,7 @@
 package esteticaapp.co.hackatec.UE;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
