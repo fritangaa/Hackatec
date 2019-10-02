7107192eb7cb5e15eb80f842741489020937bd59
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-10-02 02:30:38 -0500

    Add files via upload

---
+++ app/src/main/java/esteticaapp/co/hackatec/UE/UERegistroViaje.java
@@ -0,0 +1,16 @@
+package esteticaapp.co.hackatec.UE;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import esteticaapp.co.hackatec.R;
+
+public class UERegistroViaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ueregistro_viaje);
+
+    }
+}
